baca08659f84b633c74412f50c38b85a483236f3
Eduardo Aguad <eduaguad@gmail.com>
2017-05-20 16:34:04 -0400

    Create log folder if not exists

--- src/Log/TransbankCertificationLogger.php
+++ src/Log/TransbankCertificationLogger.php
@@ -5,45 +5,48 @@ namespace Freshwork\Transbank\Log;
 
 class TransbankCertificationLogger implements LoggerInterface
 {
-	/**
-	 * The logs directory. You need write permissions on that folder.
-	 * @var string
-	 */
-	private $log_dir;
+    /**
+     * The logs directory. You need write permissions on that folder.
+     * @var string
+     */
+    private $log_dir;
 
-	/**
-	 * TransbankCertificationLogHandler constructor.
-	 * @param string $log_dir
-	 */
-	public function __construct($log_dir = '')
-	{
-		$this->log_dir = $log_dir;
-	}
+    /**
+     * TransbankCertificationLogHandler constructor.
+     * @param string $log_dir
+     */
+    public function __construct($log_dir = '')
+    {
+        $this->log_dir = rtrim($log_dir, '/');
 
-	public function log($data, $level = self::LEVEL_INFO, $type = null)
-	{
-		if (!is_string($data)) $data = print_r($data, true);
+        if (!is_dir($this->log_dir))
+            mkdir($this->log_dir, 0775, true);
+    }
 
-		file_put_contents(
-			$this->getLogDirectory() . $this->getLogFilname(),
-			$this->getLogMessage($data, $level, $type),
-			FILE_APPEND
-			);
-	}
+    public function log($data, $level = self::LEVEL_INFO, $type = null)
+    {
+        if (!is_string($data)) $data = print_r($data, true);
 
-	protected function getLogDirectory()
-	{
-		return $this->log_dir;
-	}
+        file_put_contents(
+            $this->getLogDirectory() . '/' . $this->getLogFilname(),
+            $this->getLogMessage($data, $level, $type),
+            FILE_APPEND
+        );
+    }
 
-	protected function getLogFilname()
-	{
-		return date('Ymd') . '.transbank.log.txt';
-	}
+    protected function getLogDirectory()
+    {
+        return $this->log_dir;
+    }
 
-	protected function getLogMessage($data, $level, $type)
-	{
-		$time = date('d/m/Y H:i:s');
-		return "\n=================================\n$time - " . strtoupper($level) . " ($type):\n=================================\n$data";
-	}
+    protected function getLogFilname()
+    {
+        return date('Ymd') . '.transbank.log.txt';
+    }
+
+    protected function getLogMessage($data, $level, $type)
+    {
+        $time = date('d/m/Y H:i:s');
+        return "\n=================================\n$time - " . strtoupper($level) . " ($type):\n=================================\n$data";
+    }
 }
