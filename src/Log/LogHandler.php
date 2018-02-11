e3d834e84fb8513b5c5e4b1580fd826e0945eb82
Gonzalo De Spirito <gonzunigad@gmail.com>
2018-02-11 15:17:57 -0300

    [Logs] Fix typo

--- src/Log/LogHandler.php
+++ src/Log/LogHandler.php
@@ -5,7 +5,7 @@ namespace Freshwork\Transbank\Log;
 
 class LogHandler
 {
-	public static function log($string, $level = LoggerInterface::INFO, $type = null)
+	public static function log($string, $level = LoggerInterface::LEVEL_INFO, $type = null)
 	{
 		LoggerFactory::logger()->log($string, $level, $type);
 	}
