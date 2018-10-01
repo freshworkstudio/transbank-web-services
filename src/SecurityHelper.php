252383c4ceaebe968a28e6680cdac6d10f7bc71d
Luis Urrutia <luis@urrutia.me>
2018-09-30 23:39:49 -0300

    check if the keys and certs are file

--- src/SecurityHelper.php
+++ src/SecurityHelper.php
@@ -5,9 +5,14 @@ class SecurityHelper
 {
     public static function getCommonName($X509Cert)
     {
-        $handler = fopen($X509Cert, "r");
-        $cert = fread($handler, 8192);
-        fclose($handler);
+        if (is_file($X509Cert)) {
+            $handler = fopen($X509Cert, "r");
+            $cert = fread($handler, 8192);
+            fclose($handler);
+        } else {
+            $cert = $X509Cert;
+        }
+
         $cert_as_array = openssl_x509_parse($cert);
         return $cert_as_array['subject']['CN'];
     }
