4797f05e6d0f136631b999becb41831c19662da2
Luis Urrutia <luis@urrutia.me>
2018-11-03 13:14:00 -0300

    docs: add documentation to main classes

--- src/WebpayMall.php
+++ src/WebpayMall.php
@@ -1,10 +1,35 @@
 <?php
+/**
+ * Clase WebpayMall
+ *
+ * @package Freshwork\Transbank
+ * @author Gonzalo De Spirito <gonzunigad@gmail.com>
+ * @version 0.1.3 (07/07/2016)
+ */
+
 namespace Freshwork\Transbank;
 
+/**
+ * Clase WebpayMall
+ *
+ * @package Freshwork\Transbank
+ */
 class WebpayMall extends WebpayWebService
 {
-    public function init($returnURL, $finalURL, $buyOrder, $sessionId = null, $commerceCode = null)
+    /**
+     * Inicializa una transacción Mall en Webpay
+     *
+     * @param string $returnURL URL del comercio, a la cual Webpay redireccionará posterior al proceso de autorización
+     * @param string $finalURL URL del comercio a la cual Webpay redireccionará posterior al voucher de éxito de Webpay
+     * @param string|null $buyOrder Orden de compra de la tienda
+     * @param string|null $sessionId Identificador de sesión, uso interno de comercio
+     * @param string|null $commerceCode Código del comercio
+     *
+     * @return WebpayStandard\InitTransactionOutput
+     * @throws EmptyTransactionException
+     */
+    public function initTransaction($returnURL, $finalURL, $buyOrder, $sessionId = null, $commerceCode = null)
     {
-        return $this->initTransaction($returnURL, $finalURL, $sessionId, self::TIENDA_MALL, $buyOrder, $commerceCode);
+        return parent::initTransaction($returnURL, $finalURL, $sessionId, self::TIENDA_MALL, $buyOrder, $commerceCode);
     }
 }
--- src/WebpayWebService.php
+++ src/WebpayWebService.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Clase WebpayWebService
+ *
+ * @package Freshwork\Transbank
+ * @author Gonzalo De Spirito <gonzunigad@gmail.com>
+ * @version 0.1 (06/07/2016)
+ */
+
 namespace Freshwork\Transbank;
 
 use Freshwork\Transbank\Exceptions\EmptyTransactionException;
@@ -9,33 +17,39 @@ use Freshwork\Transbank\WebpayStandard\InitTransactionInput;
 use Freshwork\Transbank\WebpayStandard\TransactionDetail;
 
 /**
- * Class WebpayPlus
+ * Base para implementar servicios basados en Webpay Plus
+ *
+ * Clase WebpayWebService
  * @package Freshwork\Transbank
  */
 class WebpayWebService extends TransbankService
 {
+    /** @const TIENDA_NORMAL Identifica una transacción normal */
     const TIENDA_NORMAL = 'TR_NORMAL_WS';
 
+    /** @const TIENDA_MALL Identifica una trasacción de tipo Mall */
     const TIENDA_MALL = 'TR_MALL_WS';
 
+    /** @const PATPASS Indentifica una transacción PatPass */
     const PATPASS = 'TR_NORMAL_WS_WPM';
 
     /**
-     * @var WebpayStandardWebService
+     * @var WebpayStandardWebService $service Cliente SOAP involucrado en el servicio implementado
      */
     protected $service;
 
     /**
-     * @var TransactionDetail[]
+     * @var TransactionDetail[] $transactionDetails Listado con los detalles de la transacción a realizar
      */
     protected $transactionDetails = [];
 
-    /** @var  DetailInput */
+    /** @var  DetailInput $inscriptionInformation Detalles del cliente que se está suscribiendo */
     protected $inscriptionInformation;
 
     /**
-     * WebpayOneClick constructor.
-     * @param WebpayStandardWebService $service
+     * WebpayWebService constructor
+     *
+     * @param WebpayStandardWebService $service Instancia del cliente SOAP
      */
     public function __construct(WebpayStandardWebService $service)
     {
@@ -43,10 +57,11 @@ class WebpayWebService extends TransbankService
     }
 
     /**
-     * @param $amount
-     * @param $buyOrder
-     * @param null $commerceCode
+     * Añade detalles de la transacción a relizar
      *
+     * @param $amount Monto de la transacción
+     * @param $buyOrder Número de orden de compra
+     * @param null $commerceCode Código de comercio
      * @return $this
      */
     public function addTransactionDetail($amount, $buyOrder, $commerceCode = null)
@@ -63,17 +78,18 @@ class WebpayWebService extends TransbankService
     }
 
     /**
-     * @param $serviceId
-     * @param $rut
-     * @param $firstName
-     * @param $lastName1
-     * @param $lastName2
-     * @param $clientEmail
-     * @param $phoneNumber
-     * @param $expirationDate
-     * @param $commerceMail
-     * @param bool $uf
+     * Añade información del cliente y de la suscripción
      *
+     * @param string $serviceId Identificador del servicio
+     * @param string $rut RUT del cliente
+     * @param string $firstName Nombres del cliente
+     * @param string $lastName1 Apellido paterno del cliente
+     * @param string $lastName2 Apellido materno del cliente
+     * @param string $clientEmail Correo electrónico del cliente
+     * @param string $phoneNumber Teléfono del cliente
+     * @param string $expirationDate Fecha de expiración de la suscripción
+     * @param string $commerceMail Correo electrónico del comercio
+     * @param bool $uf Idica si el cobro es realizado en UF
      * @return $this
      */
     public function addInscriptionInfo(
@@ -106,17 +122,16 @@ class WebpayWebService extends TransbankService
     }
 
     /**
-     * Método que permite iniciar una transacción de pago Webpay.
+     * Inicializa una transacción en Webpay
      *
-     * @param string $returnURL URL del comercio, a la cual Webpay redireccionará posterior al proceso de autorización.
-     * @param string $finalURL URL del comercio a la cual Webpay redireccionará posterior al voucher de éxito de Webpay.
-     * @param string|null $sessionId Identificador de sesión, uso interno de comercio, este valor es
-     * devuelto al final de la transacción.
-     * @param string $transactionType
-     * @param null $buyOrder
-     * @param null $commerceCode
+     * @param string $returnURL URL del comercio, a la cual Webpay redireccionará posterior al proceso de autorización
+     * @param string $finalURL URL del comercio a la cual Webpay redireccionará posterior al voucher de éxito de Webpay
+     * @param string|null $sessionId Identificador de sesión, uso interno de comercio
+     * @param string $transactionType Indica el tipo de transacción
+     * @param string|null $buyOrder Orden de compra de la tienda
+     * @param string|null $commerceCode Código del comercio
      *
-     * @return WebpayStandard\wsInitTransactionOutput
+     * @return WebpayStandard\InitTransactionOutput
      * @throws EmptyTransactionException
      */
     public function initTransaction(
@@ -138,16 +153,15 @@ class WebpayWebService extends TransbankService
         $input->finalURL = $finalURL;
         $input->wSTransactionType = $transactionType;
         $input->transactionDetails = $this->transactionDetails;
-        $input->transactionDetails;
 
 
         return $this->service->initTransaction($input)->return;
     }
 
     /**
-     * Método que permite obtener el resultado de la transacción y los datos de la misma.
+     * Obtiene el resultado de la transacción una vez que Webpay ha resuelto su autorización financiera.
      *
-     * @param string $token
+     * @param string|null $token Token de la transacción
      * @return WebpayStandard\transactionResultOutput
      */
     public function getTransactionResult($token = null)
@@ -160,10 +174,10 @@ class WebpayWebService extends TransbankService
     }
 
     /**
-     * Método que permite informar a Webpay la correcta recepción del resultado de la transacción.
+     * Indica a Webpay que se ha recibido conforme el resultado de la transacción.
      *
-     * @param string $token
-     * @return acknowledgeTransactionResponse
+     * @param string|null $token Token de la transacción
+     * @return AcknowledgeTransactionResponse
      */
     public function acknowledgeTransaction($token = null)
     {
@@ -175,7 +189,7 @@ class WebpayWebService extends TransbankService
     }
 
     /**
-     * Assert the transactions has at least one detail
+     * Valida que la transacción tenga al menos un detalle
      *
      * @throws EmptyTransactionException
      */
@@ -183,18 +197,18 @@ class WebpayWebService extends TransbankService
     {
         if (count($this->transactionDetails) <= 0) {
             throw new EmptyTransactionException(
-                'You have to add at least one detail to the transaction with ->addDetail(...)'
+                'You have to add at least one detail to the transaction with ->addTransactionDetail(...)'
             );
         }
     }
 
     /**
-     * @param $transactionType
-     * @param $buyOrder
-     * @param $commerceCode
-     * @param $input
+     * Valida los parámetros basado en el tipo de transacción
      *
-     * @throws \InvalidArgumentException
+     * @param string $transactionType Tipo de transacción
+     * @param string $buyOrder Orden de compra de la tienda
+     * @param string $commerceCode Código de comercio
+     * @param InitTransactionInput $input Detalles del inicio de la transacción
      */
     public function validateParametersBasedOnTransactionType($transactionType, $buyOrder, $commerceCode, $input)
     {
