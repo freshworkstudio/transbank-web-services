96d9509dae60c7200896a8b8397bbb8b771228e1
Luis Urrutia <luis@urrutia.me>
2018-11-04 12:53:10 -0300

    docs: improve docs of Loggers

--- src/Log/LogHandler.php
+++ src/Log/LogHandler.php
@@ -1,11 +1,31 @@
 <?php
+/**
+ * Clase LogHandler
+ *
+ * @package Freshwork\Transbank
+ * @subpackage Log
+ * @author Gonzalo De Spirito <gonzunigad@gmail.com>
+ * @version 0.1.6 (15/07/2016)
+ */
 
 namespace Freshwork\Transbank\Log;
 
+/**
+ * Clase LogHandler
+ *
+ * @package Freshwork\Transbank\Log
+ */
 class LogHandler
 {
-    public static function log($string, $level = LoggerInterface::LEVEL_INFO, $type = null)
+    /**
+     * Registra un evento
+     *
+     * @param mixed $data Información a registrar
+     * @param string $level Tipo de evento
+     * @param mixed $type Identificador adicional
+     */
+    public static function log($data, $level = LoggerInterface::LEVEL_INFO, $type = null)
     {
-        LoggerFactory::logger()->log($string, $level, $type);
+        LoggerFactory::logger()->log($data, $level, $type);
     }
 }
