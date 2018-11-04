10e5416a6e37d30faa8573333d92ecd1069513ed
Luis Urrutia <luis@urrutia.me>
2018-11-04 12:53:40 -0300

    docs: improve docs of Exceptions

--- src/Exceptions/EmptyTransactionException.php
+++ src/Exceptions/EmptyTransactionException.php
@@ -1,6 +1,20 @@
 <?php
+/**
+ * Clase EmptyTransactionException
+ *
+ * @package Freshwork\Transbank
+ * @subpackage Exceptions
+ * @author Gonzalo De Spirito <gonzunigad@gmail.com>
+ * @version 0.1 (06/07/2016)
+ */
+
 namespace Freshwork\Transbank\Exceptions;
 
+/**
+ * Clase EmptyTransactionException
+ *
+ * @package Freshwork\Transbank\Exceptions
+ */
 class EmptyTransactionException extends \Exception
 {
 }
--- src/Exceptions/InvalidCertificateException.php
+++ src/Exceptions/InvalidCertificateException.php
@@ -1,6 +1,20 @@
 <?php
+/**
+ * Clase InvalidCertificateException
+ *
+ * @package Freshwork\Transbank
+ * @subpackage Exceptions
+ * @author Gonzalo De Spirito <gonzunigad@gmail.com>
+ * @version 0.1 (06/07/2016)
+ */
+
 namespace Freshwork\Transbank\Exceptions;
 
+/**
+ * Clase InvalidCertificateException
+ *
+ * @package Freshwork\Transbank\Exceptions
+ */
 class InvalidCertificateException extends \Exception
 {
 }
